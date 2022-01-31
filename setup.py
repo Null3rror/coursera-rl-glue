5c84df5950abd771e33ece05c591c2c2302f745b
Null3rror <69770592+Null3rror@users.noreply.github.com>
2022-01-31 12:27:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RlGlue',
-    url='https://github.com/andnp/coursera-rl-glue.git',
+    url='https://github.com/Null3rror/coursera-rl-glue.git',
     author='Andy Patterson',
     author_email='ap3@ualberta.com',
     packages=find_packages(exclude=['tests*']),
